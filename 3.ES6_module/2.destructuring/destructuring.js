ea5a9108e16f2fa7606fb465242306135138226c
Minki Kim <93183070+minki0415@users.noreply.github.com>
2022-02-21 12:22:09 +0900

    [add] ES6, javascript 신규문법 학습파일 업로드

---
+++ 3.ES6_module/2.destructuring/destructuring.js
@@ -0,0 +1,56 @@
+/**
+ * Destructuring
+ * Destructuring assignment(분해할당)은 객체의 프로퍼티나 배열을 변수로 분해해주는 것을 의미함
+ * -> 코드를 보다 간결하게 해줌.
+ * 
+ * 1. Object Destructuring
+ * 2. Array Destructuring
+ */
+
+
+
+/**
+ * 객체명 : book
+ * id : 1 
+ * title : The Great Gatsby
+ * pubDate : 10/04/1925
+ */
+
+const book = {
+    id : 1,
+    title : 'The Great Gatsby',
+    pubdate : '10/04/1925'
+}
+
+// 기존방식
+// const id = book.id;
+// const title = book.title;
+// const pubDate = book.pubdate;
+
+// 1. Object Destructuring 방식
+// const {bookId, bookTitle, bookPubDate} = book;
+// console.log(bookId, bookTitle, bookPubDate);
+
+// 결과 : undefined undefined undefined -> 변수는 객체의 프로퍼티 명과 동일한 이름으로 작성해야함.
+const {id, title, pubdate} = book;
+console.log(id, title, pubdate);
+
+// destructuring은 원본 객체를 수정하지 않음
+console.log(book);
+
+// 다른 이름으로 저장하고 싶을 경우, 
+// id -> book Id / title -> bookTitle, pubdate -> bookPubdate
+const { id : bookId, title : bookTitle, pubdate : bookPubdate} = book;
+console.log(book);
+
+
+// 2. Array Destructuring : 배열의 요소들을 개별 변수들로 분해.
+const date = ['1970', '12', '01'];
+
+// JS Array는 순서를 보장받아야하기 때문에 인덱싱 활용하였음.
+// const year = date[0];
+// const month = date[1];
+
+// Array Destructuring 방식
+const [year, month, day] = date;
+console.log(year, month, day);
---
+++ 3.ES6_module/3.module/function.js
@@ -0,0 +1,29 @@
+/**
+ * Modular Programming
+ * 
+ * 코드를 여러 파일로 쪼개서 모듈화 시키는 방식
+ * 관계있는 기능, 파일들끼리 서로 module 형태로 불러와서 필요한 코드만 사용
+ * 
+ * ES6(ECMA2015)에서 모듈화 할 수 있는 import 및 export라는 키워드를 사용함.
+ */
+
+export function sum(x, y) {
+    return x + y;
+}
+export function sub(x, y) {
+    return x - y;
+}
+export function mul(x, y) {
+    return x * y;
+}
+export function div(x, y) {
+    return x / y;
+}
+
+// Primitive values
+export const num = 100;
+export const str = 'string';
+const obj = {name : 'Homer'};
+const array = ['Bart', 'Lisa', 'Maggie'];
+
+export {obj, array};
